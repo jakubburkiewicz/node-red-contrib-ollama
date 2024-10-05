23b921a4818e677157798d55f1ac439edbbf1f54
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-10-05 12:36:54 +0200

    Refactor Ollama create function to use 'name' instead of 'model' in payload

--- nodes/ollama-create.js
+++ nodes/ollama-create.js
@@ -6,14 +6,25 @@ module.exports = function( RED ) {
         const node = this
 
         node.on( 'input', async function( msg ) {
-            const { host, model, path, modelfile, stream } = msg.payload
+            const {
+                host,
+                name,
+                modelfile,
+                stream,
+                path
+            } = msg.payload
 
             const ollama = new Ollama( { host } )
 
-            const response = await ollama.create( { model, path, modelfile, stream } )
-                .catch( error => {
-                    node.error( error )
-                } )
+            const response = await ollama.create( {
+                name,
+                modelfile,
+                stream,
+                path
+            } )
+            .catch( error => {
+                node.error( error )
+            } )
 
             msg.payload = response
             node.send( msg )
