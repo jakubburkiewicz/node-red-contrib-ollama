2f6a9b704202d4ac81cd964f27fd0abb62044890
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-12-23 12:23:50 +0100

    Refactor Ollama Create node to use msg.payload for host and model parameters

--- nodes/ollama-create.js
+++ nodes/ollama-create.js
@@ -7,20 +7,18 @@ module.exports = function( RED ) {
 
         node.on( 'input', async function( msg ) {
             const {
-                host: payloadHost,
-                model: payloadModel,
                 modelfile,
                 stream,
                 path
             } = msg.payload
 
             const server = RED.nodes.getNode( config.server )
-            const host = ( server ) ? server.host + ':' + server.port : payloadHost
+            const host = ( server ) ? server.host + ':' + server.port : msg?.payload?.host
 
             const ollama = new Ollama( { host } )
 
             const modelConfig = RED.nodes.getNode( config.model )
-            const model = ( modelConfig ) ? modelConfig.name : payloadModel
+            const model = ( modelConfig ) ? modelConfig.name : msg?.payload?.model
 
             const response = await ollama.create( {
                 model,
