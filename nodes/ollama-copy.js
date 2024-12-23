01f576c90bd2c5c3d98a063b01c4024388ffa000
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-12-23 12:27:39 +0100

    Add source and destination parameters to Ollama Copy node with type handling

--- nodes/ollama-copy.js
+++ nodes/ollama-copy.js
@@ -3,6 +3,10 @@ module.exports = function( RED ) {
 
     function OllamaCopyNode( config )  {
         RED.nodes.createNode( this, config )
+
+        this.sourceType = config.sourceType || 'str'
+        this.destinationType = config.destinationType || 'str'
+
         const node = this
         node.on( 'input', async function( msg ) {
             const server = RED.nodes.getNode( config.server )
@@ -11,7 +15,40 @@ module.exports = function( RED ) {
 
             const ollama = new Ollama( { host } )
 
-            const response = await ollama.copy( { source, destination } )
+            let source = null
+            if( !!config.source ) {
+                if( node.sourceType === 'str' ) {
+                    source = config.source
+                } else if( node.sourceType === 'msg' ) {
+                    source = msg[ config.source ]
+                } else if( node.sourceType === 'flow' ) {
+                    source = node.context().flow.get( config.source )
+                } else if( node.sourceType === 'global' ) {
+                    source = node.context().global.get( config.source )
+                }
+            } else {
+                source = msg?.payload?.source
+            }
+
+            let destination = null
+            if( !!config.destination ) {
+                if( node.destinationType === 'str' ) {
+                    destination = config.destination
+                } else if( node.destinationType === 'msg' ) {
+                    destination = msg[ config.destination ]
+                } else if( node.destinationType === 'flow' ) {
+                    destination = node.context().flow.get( config.destination )
+                } else if( node.destinationType === 'global' ) {
+                    destination = node.context().global.get( config.destination )
+                }
+            } else {
+                destination = msg?.payload?.destination
+            }
+
+            const response = await ollama.copy( {
+                    source,
+                    destination
+                } )
                 .catch( error => {
                     node.error( error )
                 } )
